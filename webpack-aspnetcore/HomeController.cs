751b9a228d7fd688fa411469caa01e76fa7d278d
Kimserey <kimserey.lam@groomgy.com>
2018-06-12 21:04:42 +0800

    added npm

---
+++ webpack-aspnetcore/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webpack_aspnetcore
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
